9b8c5d6c3f5eeab4a113c38e5cc31e0031f90b57
DevanshSharma <devansh.pec@gmail.com>
2026-02-14 23:50:34 +0100

    model building done

---
+++ src/model_building.py
@@ -0,0 +1,141 @@
+import os
+import numpy as np
+import pandas as pd
+import yaml
+import logging
+import joblib
+from dataclasses import asdict, dataclass
+from typing import Dict, Any, Tuple
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, roc_auc_score, accuracy_score
+from lightgbm import LGBMClassifier
+
+# -------------------------
+# Logging setup
+# -------------------------
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+
+logger = logging.getLogger('model_building')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'model_building.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> Dict[str, Any]:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, "r") as f:
+            params = yaml.safe_load(f)
+        logger.debug("Parameters retrieved from %s", params_path)
+        return params
+    except FileNotFoundError:
+        logger.error("File not found: %s", params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s", e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise
+
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(file_path)
+        logger.debug("Data loaded from %s with shape %s", file_path, df.shape)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error("Failed to parse the CSV file: %s", e)
+        raise
+    except FileNotFoundError as e:
+        logger.error("File not found: %s", e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error occurred while loading the data: %s", e)
+        raise
+def train_model(X_train: np.ndarray, y_train: np.ndarray, params: dict) -> LGBMClassifier:
+    """
+    Train the RandomForest model.
+    
+    :param X_train: Training features
+    :param y_train: Training labels
+    :param params: Dictionary of hyperparameters
+    :return: Trained Isolation Forest
+    """
+    try:
+        if X_train.shape[0] != y_train.shape[0]:
+            raise ValueError("The number of samples in X_train and y_train must be the same.")
+        n_estimator=25
+        random_stat=42
+        learning_rat=0.1
+        logger.debug('Initializing RandomForest model with parameters: %s', params)
+        clf = LGBMClassifier(n_estimators=n_estimator, random_state=random_stat,learning_rate=learning_rat)
+        
+        logger.debug('Model training started with %d samples', X_train.shape[0])
+        clf.fit(X_train, y_train)
+        logger.debug('Model training completed')
+        
+        return clf
+    except ValueError as e:
+        logger.error('ValueError during model training: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Error during model training: %s', e)
+        raise
+
+
+def save_model(model, file_path: str) -> None:
+    """
+    Save the trained model to a file.
+    
+    :param model: Trained model object
+    :param file_path: Path to save the model file
+    """
+    try:
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        
+        with open(file_path, 'wb') as file:
+            pickle.dump(model, file)
+        logger.debug('Model saved to %s', file_path)
+    except FileNotFoundError as e:
+        logger.error('File path not found: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Error occurred while saving the model: %s', e)
+        raise
+
+def main():
+    try:
+        # params = load_params('params.yaml')['model_building']
+        params={'n_estimators':25,'random_state':42}
+        train_data = load_data('./data/processed/train_tfidf.csv')
+        X_train = train_data.iloc[:, :-1].values
+        y_train = train_data.iloc[:, -1].values
+
+        clf = train_model(X_train, y_train,params)
+        
+        model_save_path = 'models/model.pkl'
+        save_model(clf, model_save_path)
+
+    except Exception as e:
+        logger.error('Failed to complete the model building process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
